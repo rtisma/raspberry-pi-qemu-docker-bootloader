6432ef1acd069134de4712a1b16fdd7a4b8d1a40
rtisma <rtisma@gmail.com>
2017-11-09 20:39:51 -0500

    script that runs raspberry pi emulated in a docker container

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker_image='ryankurte/docker-rpi-emu'
+# docker pull
+docker pull $docker_image
+
+relative_image_path=$1
+
+# check images exists
+if [ ! -e $relative_image_path ]; then
+    echo "The image path \"$relative_image_path\" does not exist"
+    exit 1
+fi
+abs_image_path=`readlink -f $relative_image_path`
+
+# extract parent directory
+image_dir=`dirname $abs_image_path`
+
+# extract filename
+image_file=`basename $abs_image_path`
+
+#Run docker
+docker run -it --rm --privileged=true -v $image_dir:/usr/rpi/images \
+    -w /usr/rpi $docker_image /bin/bash  \
+    -c "./run.sh images/$image_file /bin/bash"
