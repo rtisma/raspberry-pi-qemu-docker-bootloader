b4b81e1f03ebbf36cedfe679e56da17700826a07
rtisma <rtisma@gmail.com>
2018-06-14 00:23:36 -0400

    updated

--- run.sh
+++ run.sh
@@ -6,9 +6,31 @@ docker pull $docker_image
 
 relative_image_path=$1
 
+
+#  relative_data_dir=$2
+#  echo "Relative Data Dir: $relative_data_dir"
+#  if [ -z $relative_data_dir ]; then
+#      echo "The data directory \"$relative_data_dir\" was not defined"
+#      exit 1
+#  elif [ ! -e $relative_data_dir ]; then
+#      echo "The data directory \"$relative_data_dir\" does not exist"
+#      exit 1
+#  elif [ ! -d $relative_data_dir ]; then
+#      echo "The data directory \"$relative_data_dir\" is not a directory"
+#      exit 1
+#  fi
+#  absolute_data_dir=`readlink -f $relative_data_dir`
+#  echo "Absolute Data Path: $absolute_data_dir"
+
 # check images exists
-if [ ! -e $relative_image_path ]; then
+if [ -z $relative_image_path ]; then
+    echo "The image path \"$relative_image_path\" was not defined"
+    exit 1
+elif [ ! -e $relative_image_path ]; then
     echo "The image path \"$relative_image_path\" does not exist"
+    exit 1
+elif [ ! -f $relative_image_path ]; then
+    echo "The image path \"$relative_image_path\" is not a file"
     exit 1
 fi
 echo "Relative Image Path: $relative_image_path"
@@ -23,7 +45,8 @@ image_dir=`dirname $abs_image_path`
 image_file=`basename $abs_image_path`
 
 #Run docker
-docker run -it --rm --privileged=true -v $image_dir:/usr/rpi/images \
+docker run -it --rm --privileged=true \
+    -v $image_dir:/usr/rpi/images \
     -p 7777:22 \
     -w /usr/rpi $docker_image /bin/bash  \
     -c "./run.sh images/$image_file /bin/bash"
